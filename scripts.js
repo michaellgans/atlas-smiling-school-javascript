74675f8aab24a2c4ab1b178629414a2e8972da38
michaellgans <michaellgans@Victoria>
2024-04-01 13:17:39 -0500

    Changes text to API call's text

--- scripts.js
+++ scripts.js
@@ -6,9 +6,13 @@ $(document).ready(function() {
         success: function(data) {
             console.log("It's working");
 
-            /* Empties out current text */
-            $(".quote-text p").text(data[0].text);
-            console.log("Text should be changed");
+            /* Changes text on first item */
+            $("#activeQuote").text(data[0].text);
+            console.log("Text should be changed 1");
+
+            /* Changes text on second item */
+            $("#quote1").text(data[1].text);
+            console.log("Text should be changed 2");
         },
         error: function() {
             console.log("Ooops....");

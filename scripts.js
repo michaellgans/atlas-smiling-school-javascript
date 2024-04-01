d342bfab1bd255ef04b8c8cc51d2057d4f0c2325
michaellgans <michaellgans@Victoria>
2024-04-01 14:14:15 -0500

    Completes task 1

--- scripts.js
+++ scripts.js
@@ -19,14 +19,15 @@ function getQuotes() {
         success: function(data) {
             console.log("It's working");
 
+            /* Creates Active Item */
             let activeItem = `
                 <div class="carousel-item active">
                     <div class="row mx-auto align-items-center">
                         <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
                             <img
-                            src="images/profile_5.jpg"
+                            src="${data[0].pic_url}"
                             class="d-block align-self-center"
-                            alt="Carousel Pic 1"
+                            alt="Carousel Pic ${data[0].id}"
                             />
                         </div>
                         <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
@@ -34,14 +35,41 @@ function getQuotes() {
                                 <p class="text-white" id="quote1">
                                     ${data[0].text}
                                 </p>
-                            <h4 class="text-white font-weight-bold">Person Name</h4>
-                            <span class="text-white">weather presenter</span>
+                            <h4 class="text-white font-weight-bold">${data[0].name}</h4>
+                            <span class="text-white">${data[0].title}</span>
                             </div>
                         </div>
                     </div>
                 </div>`
 
             $(".carousel-inner").append(activeItem);
+
+            /* Creates Other Items */
+            for (let x = 1; x < data.length; x++) {
+                let quoteItem = `
+                <div class="carousel-item">
+                    <div class="row mx-auto align-items-center">
+                        <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
+                            <img
+                            src="${data[x].pic_url}"
+                            class="d-block align-self-center"
+                            alt="Carousel Pic ${data[x].id}"
+                            />
+                        </div>
+                        <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
+                            <div class="quote-text">
+                                <p class="text-white" id="quote1">
+                                    ${data[x].text}
+                                </p>
+                            <h4 class="text-white font-weight-bold">${data[0].name}</h4>
+                            <span class="text-white">${data[x].title}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+
+                $(".carousel-inner").append(quoteItem);
+            }
 
             hideLoader();
             console.log("Hiding loader...")
